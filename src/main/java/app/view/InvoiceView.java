bc3e0c86e2cda99e18bfd2124107c78072d57420
Leszek Rzewuski <leszek.rzewuski@envelo.pl>
2021-08-03 21:39:37 +0200

    Utworzenie metody getMainTemplateHtml w InvoiceView.java
    Metoda getMainTemplateHtml generuje szablon html

--- src/main/java/app/view/InvoiceView.java
+++ src/main/java/app/view/InvoiceView.java
@@ -18,10 +18,40 @@ public class InvoiceView {
             html.append("<tr>");
             html.append("<td>").append(++lp).append("</td>");
             html.append("<td>").append(DF_POLISH.format(invoice.getAmount())).append("</td>");
-            html.append("<td>").append(invoice.getDate().toString().substring(0, 10)).append("</td>");
+            html.append("<td>").append(invoice.getDate().toString(), 0, 10).append("</td>");
             html.append("</tr>");
         }
         html.append("</table>");
-        return html.toString();
+        return getMainTemplateHtml(html.toString());
+    }
+
+    /**
+     * Główny szablon HTML (na potrzeby amatorskiej aplikacji)
+     *
+     * @param body {@link String}
+     * @return String
+     */
+    @SuppressWarnings("SpellCheckingInspection")
+    private static String getMainTemplateHtml(String body) {
+        return "<!DOCTYPE html>\n" +
+                "<html>\n" +
+                "<head>\n" +
+                "<meta charset=\"UTF-8\">\n" +
+                "<title>My first Java web application</title>\n" +
+                "<link rel=\"stylesheet\" href=\"styles.css\">" +
+                "<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css\"" +
+                "rel=\"stylesheet\" " +
+                "integrity=\"sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC\" " +
+                "crossorigin=\"anonymous\">" +
+                "</head>\n" +
+                "<body class=\"text-center\">\n" +
+                body +
+                "<!-- JavaScript Bundle with Popper -->\n" +
+                "<script " +
+                "src=\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js\" " +
+                "integrity=\"sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM\" " +
+                "crossorigin=\"anonymous\"></script>" +
+                "</body>\n" +
+                "</html>";
     }
 }
