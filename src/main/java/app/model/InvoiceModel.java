13346ae0cf36b83f91b9ee9bad9d808a36692aea
Leszek Rzewuski <leszek.rzewuski@envelo.pl>
2021-08-05 14:45:03 +0200

    Metoda konwertująca dane z formularza na obiekt Invoice
    Metoda changeBodyToInvoice w klasie InvoiceModel

--- src/main/java/app/model/InvoiceModel.java
+++ src/main/java/app/model/InvoiceModel.java
@@ -1,2 +1,31 @@
-package app.model;public class InvoiceModel {
+package app.model;
+
+import java.util.Date;
+import java.util.Map;
+
+public class InvoiceModel {
+
+    private final InvoiceRepository repository;
+
+    public InvoiceModel(InvoiceRepository repository) {
+        this.repository = repository;
+    }
+
+    /**
+     * Zamienia dane przesłane z formularza z Map na Invoice
+     *
+     * @param body {@link Map}
+     * @return Invoice
+     */
+    public Invoice changeBodyToInvoice(Map<String, String> body) {
+        Invoice invoice = new Invoice();
+        InvoiceNumbering invoiceNumbering = new InvoiceNumbering(repository);
+        String numberInvoice = invoiceNumbering.getNextNumberInvoice();
+        Float amount = Conversion.changeToFloat(body.get("amount"));
+        Date date = Conversion.changeToDate(body.get("date"));
+        invoice.setNumberInvoice(numberInvoice);
+        invoice.setAmount(amount);
+        invoice.setDate(date);
+        return invoice;
+    }
 }
