7b2399ac462b851a83668837eeccf1ea1a255e7c
Leszek Rzewuski <leszek.rzewuski@envelo.pl>
2021-08-05 14:53:44 +0200

    Dodanie nowego Query do repozytorium
    Dodanie zapytania wyszukującego ostatni numer faktury

--- src/main/java/app/model/InvoiceRepository.java
+++ src/main/java/app/model/InvoiceRepository.java
@@ -1,6 +1,10 @@
 package app.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
+
+    @Query(value = "select number_invoice from invoice order by id desc limit 1", nativeQuery = true)
+    String getLastNumberVoice();
 }
