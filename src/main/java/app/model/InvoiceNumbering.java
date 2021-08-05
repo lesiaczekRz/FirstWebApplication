09cefb659e2374a724d3902cbf8612afd53a9027
Leszek Rzewuski <leszek.rzewuski@envelo.pl>
2021-08-05 14:49:01 +0200

    Metoda do ustalania nowego numeru faktury
    Metoda getNextNumberInvoice w klasie InvoiceNumbering

--- src/main/java/app/model/InvoiceNumbering.java
+++ src/main/java/app/model/InvoiceNumbering.java
@@ -1,2 +1,33 @@
-package app.model;public class InvoiceNumbering {
+package app.model;
+
+import java.util.Calendar;
+
+public class InvoiceNumbering {
+
+    private final InvoiceRepository repository;
+
+    public InvoiceNumbering(InvoiceRepository repository) {
+        this.repository = repository;
+    }
+
+    /**
+     * Pobiera następny numer faktury
+     *
+     * @return String|null
+     */
+    public String getNextNumberInvoice() {
+        String lastNumber = repository.getLastNumberVoice();
+        int n;
+        if (lastNumber == null) {
+            return null;
+        }
+        String year = String.valueOf(Calendar.getInstance().get(Calendar.YEAR));
+        String[] arrayNumber = lastNumber.split("/");
+        if (year.equals(arrayNumber[1])) {
+            n = Integer.parseInt(arrayNumber[0]) + 1;
+        } else {
+            n = 1;
+        }
+        return n + "/" + year;
+    }
 }
