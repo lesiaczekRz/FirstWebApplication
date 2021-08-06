725f3f5e589d868209a408582a788a9c43f33274
Leszek Rzewuski <leszek.rzewuski@envelo.pl>
2021-08-06 10:20:33 +0200

    Dodanie obsługi błędów
    Dodanie obsługi błędów do metody changeBodyToInvoice

--- src/main/java/app/model/InvoiceModel.java
+++ src/main/java/app/model/InvoiceModel.java
@@ -17,15 +17,23 @@ public class InvoiceModel {
      * @param body {@link Map}
      * @return Invoice
      */
-    public Invoice changeBodyToInvoice(Map<String, String> body) {
+    public Invoice changeBodyToInvoice(Map<String, String> body) throws WrongParameter {
         Invoice invoice = new Invoice();
         InvoiceNumbering invoiceNumbering = new InvoiceNumbering(repository);
         String numberInvoice = invoiceNumbering.getNextNumberInvoice();
         Float amount = Conversion.changeToFloat(body.get("amount"));
         Date date = Conversion.changeToDate(body.get("date"));
         invoice.setNumberInvoice(numberInvoice);
-        invoice.setAmount(amount);
-        invoice.setDate(date);
+        if (amount == null) {
+            throw new WrongParameter("Błędny format kwoty");
+        } else {
+            invoice.setAmount(amount);
+        }
+        if (date == null) {
+            throw new WrongParameter("Błędny format daty");
+        } else {
+            invoice.setDate(date);
+        }
         return invoice;
     }
 }
