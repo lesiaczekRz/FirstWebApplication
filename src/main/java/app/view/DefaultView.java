41b24ed28dcd9cf57919bb426fd46e54e8b9cd49
Leszek Rzewuski <leszek.rzewuski@envelo.pl>
2021-08-23 13:59:20 +0200

    Korekta adresu do styles.css

--- src/main/java/app/view/DefaultView.java
+++ src/main/java/app/view/DefaultView.java
@@ -15,7 +15,7 @@ public abstract class DefaultView {
                 "<head>\n" +
                 "<meta charset=\"UTF-8\">\n" +
                 "<title>My first Java web application</title>\n" +
-                "<link rel=\"stylesheet\" href=\"styles.css\">" +
+                "<link rel=\"stylesheet\" href=\"/styles.css\">" +
                 "<link href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css\"" +
                 "rel=\"stylesheet\" " +
                 "integrity=\"sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC\" " +
